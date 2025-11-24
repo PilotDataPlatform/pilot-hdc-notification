40aa7cd3379e17088000feda4416b760828c400f
Vadym Moshynskyi <vmoshynskyi@indocresearch.org>
2025-11-24 17:56:16 +0100

    IEBH-353: Sync with the latest changes

--- migrations/versions/0010_add_tables_related_to_announcements.py
+++ migrations/versions/0010_add_tables_related_to_announcements.py
@@ -3,7 +3,6 @@
 # Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE,
 # Version 3.0 (the "License") available at https://www.gnu.org/licenses/agpl-3.0.en.html.
 # You may not use this file except in compliance with the License.
-
 """Add tables related to announcements.
 
 Revision ID: 0010
--- migrations/versions/f7b7903f78c4_add_system_maintenance_table.py
+++ migrations/versions/f7b7903f78c4_add_system_maintenance_table.py
@@ -3,7 +3,6 @@
 # Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE,
 # Version 3.0 (the "License") available at https://www.gnu.org/licenses/agpl-3.0.en.html.
 # You may not use this file except in compliance with the License.
-
 """Add system_maintenance table.
 
 Revision ID: f7b7903f78c4
